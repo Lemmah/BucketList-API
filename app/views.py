bd417bef38c0212860ea32d417abc6d5f478e4ea
Lemmah <lemayiannakolah@gmail.com>
2017-07-27 11:46:16 +0300

    [chore] Defining endpoints

--- app/views.py
+++ app/views.py
@@ -1,3 +1,30 @@
 
 from app import app
 
+# User auth
+@app.route('/api/auth/register', methods=['POST'])
+
+@app.route('/api/auth/login', methods=['POST'])
+
+@app.route('/api/auth/logout', methods=['POST'])
+
+@app.route('/api/auth/reset-password', methods=['POST'])
+
+# CRUD bucketlist
+@app.route('/api/bucketlists/', methods=['POST'])
+
+@app.route('/api/bucketlists/', methods=['GET'])
+
+@app.route('/api/bucketlists/<id>', methods=['GET'])
+
+@app.route('/api/bucketlists/<id>', methods=['PUT'])
+
+@app.route('/api/bucketlists/<id>', methods=['DELETE'])
+
+# CRUD bucketlist items
+@app.route('/api/bucketlists/<id>/items/', methods['POST'])
+
+@app.route('/api/bucketlists/<id>/items/<item_id>', methods['PUT'])
+
+@app.route('/api/bucketlists/<id>/items/<item_id>', methods=['DELETE'])
+
