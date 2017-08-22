9d183ca3f789cea16deac527f163123aaf354461
Lemmah <lemayiannakolah@gmail.com>
2017-08-23 00:32:02 +0300

    [chore] Reduce Syntactical Overhead in Global Var

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,10 @@ from instance.config import app_config
 db = SQLAlchemy()
 
 def create_app(config_name):
-    globals()['app'] = FlaskAPI(__name__, instance_relative_config=True)
+	# make app global so that it can be imported
+	# in the views
+    global app
+    app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -18,6 +21,6 @@ def create_app(config_name):
 
     # Load the API endpoints
     from app import views
-    
+
     return app
 
