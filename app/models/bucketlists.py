7d81922b7f122c670b12be5d40e185dc53177482
Lemmah <lemayiannakolah@gmail.com>
2017-08-23 02:46:52 +0300

    [chore] Solve Circular Imports

--- app/models/bucketlists.py
+++ app/models/bucketlists.py
@@ -1,6 +1,44 @@
-# app/models/bucketlists.py
+# app/models/users
 
 from app import db
+from flask_bcrypt import Bcrypt
+
+class User(db.Model):
+    """This class defines the users table """
+
+    __tablename__ = 'users'
+
+    # Define the columns of the users table, starting with the primary key
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(256), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+    bucketlists = db.relationship(
+        'Bucketlist', order_by='Bucketlist.id', cascade="all, delete-orphan")
+
+    def __init__(self, email, password):
+        """Initialize the user with an email and a password."""
+        self.email = email
+        self.password = Bcrypt().generate_password_hash(password).decode()
+
+    def password_is_valid(self, password):
+        """
+        Checks the password against it's hash to validates the user's password
+        """
+        return Bcrypt().check_password_hash(self.password, password)
+
+    def save(self):
+        """Save a user to the database.
+        This includes creating a new user and editing one.
+        """
+        db.session.add(self)
+        db.session.commit()
+
+
+# app/models/bucketlists
+
+from app import db
+#for the sake of relationships
+from app.models.users import User
 
 class Bucketlist(db.Model):
     """This class represents the bucketlist table."""
@@ -14,22 +52,21 @@ class Bucketlist(db.Model):
         db.DateTime, default=db.func.current_timestamp(),
         onupdate=db.func.current_timestamp())
     public_id = db.Column(db.String(255))
-    user_id = db.Column(db.String(255))
+    created_by = db.Column(db.Integer, db.ForeignKey(User.id))
 
-    def __init__(self, name, public_id, user_id="current user"):
+    def __init__(self, name, public_id, created_by):
         """initialize with name, public_id and user_id"""
         self.name = name
         self.public_id = public_id
-        self.user_id = user_id
+        self.created_by = created_by
 
     def save(self):
         db.session.add(self)
         db.session.commit()
 
     @staticmethod
-    def get_all():
-        return Bucketlist.query.all()
-
+    def get_all(user_id):
+        return Bucketlist.query.filter_by(created_by=user_id)
     def delete(self):
         db.session.delete(self)
         db.session.commit()
