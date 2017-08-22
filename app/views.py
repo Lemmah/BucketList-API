d72547c7b2954bb16af82eff9322115ca97beb52
Lemmah <lemayiannakolah@gmail.com>
2017-08-22 23:27:28 +0300

    [chore] Move Endpoints to a Views file

--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
 
-from app import app
-
+""" REQUIRED ENDPOINTS
 # User auth
 @app.route('/api/auth/register', methods=['POST'])
 
@@ -30,3 +29,75 @@ from app import app
 def delete_item(id,item_id):
     ''' Deleting a bucketlist item '''
     return 'This feature is yet to be implemented'
+"""
+from app import app
+from app.models import Bucketlist
+from flask import request, jsonify, abort
+
+@app.route('/bucketlists/', methods=['POST', 'GET'])
+def bucketlists():
+    if request.method == "POST":
+        name = str(request.data.get('name', ''))
+        if name:
+            bucketlist = Bucketlist(name=name)
+            bucketlist.save()
+            response = jsonify({
+                'id': bucketlist.id,
+                'name': bucketlist.name,
+                'date_created': bucketlist.date_created,
+                'date_modified': bucketlist.date_modified
+            })
+            response.status_code = 201
+            return response
+    else:
+        # GET
+        bucketlists = Bucketlist.get_all()
+        results = []
+
+        for bucketlist in bucketlists:
+            obj = {
+                'id': bucketlist.id,
+                'name': bucketlist.name,
+                'date_created': bucketlist.date_created,
+                'date_modified': bucketlist.date_modified
+            }
+            results.append(obj)
+        response = jsonify(results)
+        response.status_code = 200
+        return response
+@app.route('/bucketlists/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def bucketlist_manipulation(id, **kwargs):
+ # retrieve a buckelist using it's ID
+    bucketlist = Bucketlist.query.filter_by(id=id).first()
+    if not bucketlist:
+        # Raise an HTTPException with a 404 not found status code
+        abort(404)
+
+    if request.method == 'DELETE':
+        bucketlist.delete()
+        return {
+        "message": "bucketlist {} deleted successfully".format(bucketlist.id) 
+     }, 200
+
+    elif request.method == 'PUT':
+        name = str(request.data.get('name', ''))
+        bucketlist.name = name
+        bucketlist.save()
+        response = jsonify({
+            'id': bucketlist.id,
+            'name': bucketlist.name,
+            'date_created': bucketlist.date_created,
+            'date_modified': bucketlist.date_modified
+        })
+        response.status_code = 200
+        return response
+    else:
+        # GET
+        response = jsonify({
+            'id': bucketlist.id,
+            'name': bucketlist.name,
+            'date_created': bucketlist.date_created,
+            'date_modified': bucketlist.date_modified
+        })
+        response.status_code = 200
+        return response
