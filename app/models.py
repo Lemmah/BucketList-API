09b8d104be257e0b10f67974dcff8dc36e77e7fa
Lemmah <lemayiannakolah@gmail.com>
2017-08-22 17:10:58 +0300

    [chore] Develop New BucketList Model

--- app/models.py
+++ app/models.py
@@ -1,39 +1,34 @@
-# app loads configurations here from the config file
-from app import app
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+# app/models.py
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://bucketlist_api:easyPassword@localhost:5432/bucketlist_app"
+from app import db
 
-db = SQLAlchemy(app)
+class Bucketlist(db.Model):
+    """This class represents the bucketlist table."""
 
+    __tablename__ = 'bucketlists'
 
-class User(db.Model):
-    ''' A model of the User Table '''
-    user_id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(80), unique=True)
-    username = db.Column(db.String(60))
-    password = db.Column(db.String(256))
-    secure_id = db.Column(db.String(60), unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(
+        db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
 
+    def __init__(self, name):
+        """initialize with name."""
+        self.name = name
 
-class BucketList(db.Model):
-    ''' A model of the BucketList Table '''
-    bucketlist_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(60))
-    description = db.Column(db.String(100))
-    # foreign key to user: from User.secure_id
-    user_sid = db.Column(db.String(60))
-    secure_id = db.Column(db.String(60), unique=True)
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
+    @staticmethod
+    def get_all():
+        return Bucketlist.query.all()
 
-class BucketListItem(db.Model):
-    ''' A model of the BucketListItem Table '''
-    item_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(60))
-    category = db.Column(db.String(30))
-    description = db.Column(db.String(100))
-    accomplished = db.Column(db.Boolean)
-    # foreign key to bucketlist: from Bucketlist.secure_id
-    bucketlist_sid = db.Column(db.String(60), unique=True)
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
+    def __repr__(self):
+        return "<Bucketlist: {}>".format(self.name)
