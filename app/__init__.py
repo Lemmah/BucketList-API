e9ad82ed350bb26159c60f8739404cc1de33e21c
Lemmah <lemayiannakolah@gmail.com>
2017-08-22 17:09:42 +0300

    [chore] Clean up App init

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,19 @@
 # app/__init__.py
 
-from flask import Flask
+from flask_api import FlaskAPI
+from flask_sqlalchemy import SQLAlchemy
 
-# Initialize the app
-app = Flask(__name__, instance_relative_config=True)
+# local import
+from instance.config import app_config
 
-# Load the views
-from app import views
+# initialize sql-alchemy
+db = SQLAlchemy()
 
-# Load the models
-from app import models
+def create_app(config_name):
+    app = FlaskAPI(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
 
-# Load the config file
-app.config.from_object('config')
+    return app
