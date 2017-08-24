b08c2ba1e836e565c5780e91739e73a813b89fd9
Lemmah <lemayiannakolah@gmail.com>
2017-08-24 19:43:51 +0300

    [fix] Handle KeyError - Failing to Create a BL

--- tests/test_bucketlist_item.py
+++ tests/test_bucketlist_item.py
@@ -7,24 +7,33 @@ class TestBucketlistItem(test_bucketlist.BucketlistTestCase):
     Testing Bucketlist Items which are in a bucketlist
     """
 
-    def create_bucketlist_item(self, item_details):
+    def create_bucketlist_item(self, *args):
         """ Important for Creation of items multiple times """
         res, access_token = self.create_bucketlist()
-        self.assertEqual(res.status_code, 201)
         results = json.loads(res.data.decode())
+        try:
+            bucketlist_id = results['id']
+        # Occurence of KeyError means that:
+        # the request just returned an error
+        # I expect a conflict error if the item already exists
+        except KeyError:
+            bucketlist_id = args[1]
 
         # Request to add an item to it
         rv = self.client().post(
-            '/bucketlists/{}/items'.format(results['id']),
+            '/bucketlists/{}/items'.format(bucketlist_id),
             headers=dict(Authorization="Bearer " + access_token),
             data=self.bucketlist_item)
-        return (res, access_token, rv)
+        return (access_token, rv, bucketlist_id)
 
     def test_bucketlist_item_creation(self):
         """ Test if API can add items to bucketlist """
         # First, Create a Bucketlist
-        res, access_token, rv = self.create_bucketlist_item(self.bucketlist_item)
+        rv, bucketlist_id = self.create_bucketlist_item(self.bucketlist_item)[1:3]
         self.assertEqual(rv.status_code, 201)
+        self.assertIn('See the Hills There', str(rv.data))
+        rv = self.create_bucketlist_item(self.bucketlist_item, bucketlist_id)[1]
+        self.assertEqual(rv.status_code, 409)
 
 
     def test_api_can_get_all_buckelist_items(self):
