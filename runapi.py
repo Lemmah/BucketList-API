2106a1726e8112c17d06422ecfdcba23d87799fd
Lemmah <lemayiannakolah@gmail.com>
2017-08-25 11:56:01 +0300

    [chore] Add Port to RunApi

--- runapi.py
+++ runapi.py
@@ -6,4 +6,5 @@ config_name = os.getenv('APP_SETTINGS') # config_name = "development"
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run()
+	port = int(os.environ.get('PORT', 5000))
+	app.run('', port=port)
