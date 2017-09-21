74e7569ea164f2a557d8c2982e3ae2e4dc82035c
Lemmah <lemayiannakolah@gmail.com>
2017-09-21 11:34:24 +0300

    [fix] Pass self Instance to be Accessed at Runtime

--- app/decorators.py
+++ app/decorators.py
@@ -6,7 +6,7 @@ from app.responses.responses import Response, Success, Error
 
 def token_required(f):
     @wraps(f)
-    def decorated(*args, **kwargs):
+    def decorated(self, *args, **kwargs):
         access_token = None
         if 'Authorization' in request.headers:
             auth_header = request.headers.get('Authorization')
@@ -16,6 +16,6 @@ def token_required(f):
         "Login to get authorized. If you had logged in, your session expired.")
         user_id = User.decode_token(access_token)
         if isinstance(user_id, str):
-            return self.error.forbid_action("Token has been rejected")
-        return f(*args, user_id=user_id, **kwargs)
+            return self.error.forbid_action("Token has been rejected.")
+        return f(self, *args, user_id=user_id, **kwargs)
     return decorated
